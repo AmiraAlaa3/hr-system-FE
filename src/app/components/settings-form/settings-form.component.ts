4eec427806ad31edb4499821d83521c32834c301
Amira Ala <ameraalaa641@gmail.com>
2024-09-16 01:18:43 +0300

    create setting form

--- src/app/components/settings-form/settings-form.component.ts
+++ src/app/components/settings-form/settings-form.component.ts
@@ -1,12 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageTitleComponent } from '../page-title/page-title.component';
+import { GeneralSettingService } from '../../services/generalSetting.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-settings-form',
   standalone: true,
-  imports: [],
+  imports: [PageTitleComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './settings-form.component.html',
-  styleUrl: './settings-form.component.css'
+  styleUrls: ['./settings-form.component.css'],
 })
-export class SettingsFormComponent {
+export class SettingsFormComponent implements OnInit {
+  settingId: number = 1;
+  messages: string = '';
+  errors: string = '';
+  daysOfWeek: string[] = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  settingForm = new FormGroup({
+    weekend1: new FormControl('', [Validators.required]),
+    weekend2: new FormControl('', [Validators.required]),
+    bonusHours: new FormControl(1, [Validators.required, Validators.min(1)]),
+    deductionsHours: new FormControl(1, [
+      Validators.required,
+      Validators.min(1),
+    ]),
+  });
 
+  constructor(
+    private generalSettingService: GeneralSettingService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.generalSettingService.getGeneralSettings().subscribe({
+      next: (response) => {
+        const setting = response.data[0];
+        if (setting) {
+          this.settingForm.setValue({
+            weekend1: setting.weekend1 ?? 'Friday',
+            weekend2: setting.weekend2 ?? 'Saturday',
+            bonusHours: setting.bonusHours ?? 1,
+            deductionsHours: setting.deductionsHours ?? 1,
+          });
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching settings:', err);
+      },
+    });
+  }
+
+  get getWeekend1() {
+    return this.settingForm.get('weekend1')!;
+  }
+
+  get getWeekend2() {
+    return this.settingForm.get('weekend2')!;
+  }
+
+  get getBonusHours() {
+    return this.settingForm.get('bonusHours')!;
+  }
+
+  get getDeductionsHours() {
+    return this.settingForm.get('deductionsHours')!;
+  }
+
+  settingHandler(e: Event) {
+    e.preventDefault();
+    if (this.settingForm.valid) {
+      this.generalSettingService
+        .updateGeneralSetting(this.settingId, this.settingForm.value)
+        .subscribe({
+          next: () => {
+            this.messages = 'Settings updated successfully!';
+            this.router.navigate(['/settings']);
+            this.hideMessagesAfterDelay();
+          },
+          error: (error) => {
+            this.errors = error.error.error;
+            this.hideMessagesAfterDelay();
+          },
+        });
+    } else {
+      this.settingForm.markAllAsTouched();
+    }
+  }
+
+  hideMessagesAfterDelay() {
+    setTimeout(() => {
+      this.messages = '';
+      this.errors = '';
+    }, 3000); 
+  }
 }
